13eefdb7ffc3f8790c5cdc516d85b481514d529e
cristianolivera1 <oliverachavezcristian@gmail.com>
2026-05-20 22:39:06 -0500

    feat: add react-router-dom dependency and update base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
-    server: {
-    host: true,   
-    port: 5173       
+  base: '/',
+  server: {
+    host: true,
+    port: 5173
   }
-
 })
